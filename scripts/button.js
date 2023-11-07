89ca8ab427ab30e139d0ceb4a3a7e895f7a546e6
Namson Pham <namsonpham@gmail.com>
2023-11-07 15:47:29 -0600

    added surprise.html

--- scripts/button.js
+++ scripts/button.js
@@ -33,3 +33,20 @@ function showLoginAlert() {
         }
     }
 }
+
+function showLoginAlertSurprise() {
+    var password = prompt("enter the code baby: ");
+    var correctPasswordArr = ["10/08/2023", "10/8/2023", "10/8/23", "10/08/23"];
+    if (correctPasswordArr.includes(password)) {
+        alert("happy 1 month anniversary :)");
+        window.location.href = "/surprise.html";
+        attempts = 0;
+    } else {
+        if (attempts > maxAttempts) {
+            alert("hint: it's a date 🙄");
+        } else {
+            alert("you're not jho, get out >:(");
+            attempts++;
+        }
+    }
+}
