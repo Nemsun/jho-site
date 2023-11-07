125f6e0f79fd5802b2b56abd84693c15e05dfd48
Namson Pham <namsonpham@gmail.com>
2023-11-07 09:41:05 -0600

    added password to access certain parts of site

--- button.js
+++ button.js
@@ -13,3 +13,23 @@ function reset() {
         messageEl[0].remove()
     }
 }
+
+var attempts = 0;
+var maxAttempts = 3;
+
+function showLoginAlert() {
+    var password = prompt("enter the code baby: ");
+    var correctPasswordArr = ["10/08/2023", "10/8/2023", "10/8/23", "10/08/23"];
+    if (correctPasswordArr.includes(password)) {
+        alert("happy 1 month anniversary :)");
+        window.location.href = "/jhonna.html";
+        attempts = 0;
+    } else {
+        if (attempts > maxAttempts) {
+            alert("hint: it's a date 🙄");
+        } else {
+            alert("you're not jho, get out >:(");
+            attempts++;
+        }
+    }
+}
